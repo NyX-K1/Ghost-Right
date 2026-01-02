3c2c21c75cce63090a9d5c95a9a527282e6e26c2
NyX-K1 <nyx59247@gmail.com>
2026-01-02 07:27:15 +0000

    refactor: moved local n8n_data to external storage

--- n8n_data/.cache/n8n/public/assets/auth.eventBus-Pqv7Rxfm.js
+++
@@ -1,5 +0,0 @@
-import { T as createEventBus } from "./truncate-Dmdv43Z9.js";
-const confirmPasswordEventBus = createEventBus();
-const mfaEventBus = createEventBus();
-const promptMfaCodeBus = createEventBus();
-export { mfaEventBus as n, promptMfaCodeBus as r, confirmPasswordEventBus as t };
--- n8n_data/.cache/n8n/public/assets/sourceControl.eventBus-dh45B_eQ.js
+++
@@ -1,3 +0,0 @@
-import { T as createEventBus } from "./truncate-Dmdv43Z9.js";
-const sourceControlEventBus = createEventBus();
-export { sourceControlEventBus as t };
